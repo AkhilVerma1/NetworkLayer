ad897e7f5bf10956e21b75c9c77fb0143b4a5655
Akhil Verma <42695861+AkhilVerma1@users.noreply.github.com>
2024-04-29 17:07:21 +0530

    Usage example added

--- NetworkLayer/ContentView.swift
+++ NetworkLayer/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let viewModel = ViewModel()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Fetch Users") {
+                Task {
+                    await viewModel.fetch()
+                }
+            }
         }
         .padding()
     }
@@ -21,4 +23,48 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+//usage
+
+class ViewModel {
+    
+    func fetch() async {
+        let request = OLRequestType(baseURL: URL(string: "https://api.github.com")!,
+                                    path: "users",
+                                    httpMethod: .get,
+                                    task: .request)
+
+        let result = try? await OLAPIFetcher
+            .init()
+            .withAPI(request)
+            .fetch(OLGithubTestResponseModel.self)
+        
+        debugPrint(result?.responseModel?.githubUsers.first?.id ?? 0)
+    }
+}
+
+class OLGithubTestResponseModel: OLAPIResponse {
+    var githubUsers: [OLGithubUserAPIModel] = []
+
+    required init(_ response: Any?) {
+        guard let response = response as? [[String: Any]] else { return }
+        for user in response {
+            githubUsers.append(OLGithubUserAPIModel(user))
+        }
+    }
+}
+
+class OLGithubUserAPIModel {
+    var id: Int?
+    var login: String?
+    var nodeId: String?
+    var avatarUrl: String?
+
+    init(_ response: [String: Any]) {
+        id = response["id"] as? Int
+        login = response ["login"] as? String
+        nodeId = response ["node_id"] as? String
+        avatarUrl = response ["avatar_url"] as? String
+    }
 }
